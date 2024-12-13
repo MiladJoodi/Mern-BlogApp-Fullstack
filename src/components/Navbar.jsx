56f4a1534286dc3d7a2eb74dbfa52496f6cff36f
Milad Joodi <winbeta.com@gmail.com>
2024-12-13 22:24:07 +0330

    React Router Dom ✔️

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,58 @@
 import { useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [open, setOpen] = useState(false);
-
 
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
-      <div className="flex items-center gap-4 text-2xl font-bold">
-        <img src="/logo.png" className="w-8 h-8" alt="logo" />
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.png" alt="logo" w={32} h={32} />
         <span>MyBlog</span>
-      </div>
+      </Link>
 
       {/* MOBILE MENU */}
       <div className="md:hidden">
         {/* Mobile button */}
-        <div className="cursor-pointer text-4xl" onClick={()=> setOpen((prev)=> !prev)}>
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           {open ? "×" : "≡"}
         </div>
 
-          {/* Mobile Links List */}
-        <div className={`w-full h-screen flex flex-col items-center gap-8 font-medium text-lg justify-center absolute top-16 transition-all ease-in-out  ${open ? "-right-0" : "-right-[100%]"}`}>
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
-        <button className="py-2 px-3 rounded-3xl bg-blue-800 text-white">Login 👋</button>
-        </a>
+        {/* Mobile Links List */}
+        <div
+          className={`w-full h-screen flex flex-col items-center gap-8 font-medium text-lg justify-center absolute top-16 transition-all ease-in-out  ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link Link to="/">Home</Link>
+          <Link Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
+            <button className="py-2 px-3 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
         </div>
       </div>
 
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
-        <button className="py-2 px-3 rounded-3xl bg-blue-800 text-white">Login 👋</button>
-        </a>
+      <Link Link to="/">Home</Link>
+          <Link Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
+            <button className="py-2 px-3 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
       </div>
     </div>
   );
